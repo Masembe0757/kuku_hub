f12b58250077f8c0d2faf5776f7ba20df1cb5e58
Masembe0757 <sendyj886@gmail.com>
2026-01-18 17:38:55 +0900

    Add immersive mode to hide system navigation bars

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Platform } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import * as NavigationBar from 'expo-navigation-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { AppProvider } from './src/context/AppContext';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      NavigationBar.setVisibilityAsync('hidden');
+      NavigationBar.setBehaviorAsync('overlay-swipe');
+    }
+  }, []);
+
   return (
     <SafeAreaProvider>
       <AppProvider>
-        <StatusBar style="light" />
+        <StatusBar style="light" hidden />
         <AppNavigator />
       </AppProvider>
     </SafeAreaProvider>
